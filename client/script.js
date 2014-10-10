f3587ebbb17232d4afccc0c1b793352cb470f886
John Wallace <jwallace@appneta.com>
2014-10-10 09:51:15 -0400

    chat no full screen

--- client/script.js
+++ client/script.js
@@ -42,14 +42,14 @@ $(document).ready(function() {
     // });
 
 
-    $(document).mouseup(function(e) {
-        var container = $(".chat-wrap");
+    // $(document).mouseup(function(e) {
+    //     var container = $(".chat-wrap");
 
-        if (!container.is(e.target) // if the target of the click isn't the container...
-            && container.has(e.target).length === 0) // ... nor a descendant of the container
-        {
-            container.hide();
-            $('.chat-open').show();
-        }
-    });
+    //     if (!container.is(e.target) // if the target of the click isn't the container...
+    //         && container.has(e.target).length === 0) // ... nor a descendant of the container
+    //     {
+    //         container.hide();
+    //         $('.chat-open').show();
+    //     }
+    // });
 });
