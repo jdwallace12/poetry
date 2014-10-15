844b18130d74799eb0a283845a046ee5f753514e
John Wallace <jdwallace12@gmail.com>
2014-10-14 23:02:04 -0400

    git ignore

---
+++ .meteor/local/build/programs/web.browser/app/client/script.js
@@ -0,0 +1,57 @@
+(function(){$(document).ready(function() {
+    $('.chat-wrap').hide();
+    $('.chat-open').show();
+    $('#close_chat').on('click', function() {
+        $('.chat-wrap').hide();
+        $('.chat-open').show();
+    });
+    $('.chat-open').on('click', function() {
+        $('.chat-open').hide();
+        $('.chat-wrap').show();
+        $('#newMessage').focus();
+    });
+
+    // closes modal when escape key is pressed
+    var KEYCODE_ESC = 27;
+    $(document).keyup(function(e) {
+        if (e.keyCode == KEYCODE_ESC) {
+            $('#close_chat').click();
+        }
+    });
+
+
+    $('#send').keydown(function(e){
+        if(e.keyCode==13) e.preventDefault();
+    }); 
+
+    $('#send').keyup(function(e) {
+        e.preventDefault();
+        var $textbox = $(this);
+        if($textbox.val().length > 0 && e.keyCode == 13) {
+            $('#send').click();
+        }
+     });
+
+
+    // var KEYCODE_ENTER = 13;
+    // $(document).keyup(function(e) {
+    //     if (e.keyCode == KEYCODE_ENTER) {
+    //         $('#send').click();
+    //      return false;
+    //     }
+    // });
+
+
+    // $(document).mouseup(function(e) {
+    //     var container = $(".chat-wrap");
+
+    //     if (!container.is(e.target) // if the target of the click isn't the container...
+    //         && container.has(e.target).length === 0) // ... nor a descendant of the container
+    //     {
+    //         container.hide();
+    //         $('.chat-open').show();
+    //     }
+    // });
+});
+
+})();
