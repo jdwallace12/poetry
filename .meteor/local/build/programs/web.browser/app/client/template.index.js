cc53f4e9f64d4b49c41a00fd9c493b215e20d9a6
John Wallace <jwallace@appneta.com>
2014-11-04 14:39:20 -0500

    sharethis buttons working on dynamic calls

--- .meteor/local/build/programs/web.browser/app/client/template.index.js
+++ .meteor/local/build/programs/web.browser/app/client/template.index.js
@@ -1,7 +1,11 @@
 (function(){
 Template.body.addContent((function() {
   var view = this;
-  return "";
+  return HTML.SCRIPT({
+    type: "text/javascript",
+    src: "//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5459272168788aab",
+    async: "async"
+  });
 }));
 Meteor.startup(Template.body.renderToDocument);
 
--- client/router.js
+++ client/router.js
@@ -4,5 +4,4 @@ Router.map(function(){
     this.route('home', {path: '/'} );
   user = Meteor.userId();
     this.route('private', {path:'/' + user });
-  
 });
