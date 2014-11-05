568a70edc4dcb550090dceed6bf675a313b964d8
John Wallace <jwallace@appneta.com>
2014-11-05 16:17:32 -0500

    logo on right side of fridge

--- .meteor/local/build/programs/web.browser/app/client/template.index.js
+++ .meteor/local/build/programs/web.browser/app/client/template.index.js
@@ -1,11 +1,7 @@
 (function(){
 Template.body.addContent((function() {
   var view = this;
-  return HTML.SCRIPT({
-    type: "text/javascript",
-    src: "//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5459272168788aab",
-    async: "async"
-  });
+  return "";
 }));
 Meteor.startup(Template.body.renderToDocument);
 
