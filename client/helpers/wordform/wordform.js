dcad5359c35208943ce3337bf9564a0ea897b212
John Wallace <jwallace@appneta.com>
2014-11-12 18:40:26 -0500

    swear filter is working client side

--- client/helpers/wordform/wordform.js
+++ client/helpers/wordform/wordform.js
@@ -4,16 +4,17 @@ Template.wordForm.events = {
         // Don't postback
         e.preventDefault();
         // add the word to the db  
-    $('.magnet').profanityFilter({
-    customSwears: ['shit', 'fuck'],
-    replaceWith: ['fiddle', 'fun', 'stupendous']
-});   
         Magnets.insert({
             name: tmpl.find("#name_of_word").value,
             top: Math.floor(Random.fraction() * 100) * 7,
             left: Math.floor(Random.fraction() * 100) * 8           
         });
+        
+    $('.magnet').profanityFilter({
+    customSwears: ['shit', 'fuck', 'ass', 'cunt', 'niggers'],
+    replaceWith: ['fiddle', 'fun', 'stupendous', 'puppies', 'happy']
 
+    });   
 
          $('.magnet:last-child').effect('highlight', {color: "#f1c40f;"}, 1300);
            var slap = $('#slap')[0]
