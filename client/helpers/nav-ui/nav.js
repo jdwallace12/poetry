b25a331c8a93d987a247d077f17b33b488762093
John Wallace <jwallace@appneta.com>
2014-11-20 11:11:40 -0500

    dropdown for magnet sets working

---
+++ client/helpers/nav-ui/nav.js
@@ -0,0 +1,53 @@
+  $(document).ready(function() {
+
+            $(document).on("click", "#add_words", function() {
+            $('#add_words').toggleClass('active');
+            var pop = $('#pop')[0]
+            pop.play();
+            pop.volume = .3;
+            $('#footer').slideToggle(150);
+            $('#name_of_word').focus();
+        });
+
+
+        $(document).on("click", "#delete", function() {
+            var crumple = $('#crumple')[0]
+            crumple.play();
+            crumple.volume = .3;
+        });
+
+        stButtons.locateElements();
+
+        // or if you want to be a bit defensive about whether the lib has been
+        // loaded or not:
+        if (window.stButtons) {
+            stButtons.locateElements();
+        } // Parse ShareThis markup
+
+        $("#logo_magnet").draggable();
+
+        $(document).on("click", "#chat-open", function() {
+            $('#chat-open').toggleClass('active');
+            var pop = $('#pop')[0]
+            pop.play();
+            pop.volume = .3;
+            $('.chat-wrap').toggle('slide', {
+                direction: 'left'
+            }, 200);
+            $('#newMessage').focus();
+        });
+
+
+        $(document).on("click", "#word-sets-trigger", function() {
+            $('#word-sets').toggleClass('active');
+            var pop = $('#pop')[0]
+            pop.play();
+            pop.volume = .3;
+        });
+
+
+     $(document).on("click", "login-buttons-logout", function() {
+        $('#loginModal').modal('hide');
+     });    
+
+    });
