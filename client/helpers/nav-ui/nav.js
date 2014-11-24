ecd0a92f466ef31c6621a0cb4ad6a6584c15565e
John Wallace <jwallace@appneta.com>
2014-11-24 13:38:21 -0500

    no more modal login

--- client/helpers/nav-ui/nav.js
+++ client/helpers/nav-ui/nav.js
@@ -72,47 +72,46 @@
 
 
 
-       $(document).on('click', "#login-sign-in-link, .login-buttons", function (){
-        $('.nav-collapse').css('height', '405px');
-       }); 
+       $(document).on('click', "#login-sign-in-link, .login-buttons", function() {
+           $('.nav-collapse').css('height', '405px');
+       });
 
 
-     $(document).on('click', "#signup-link", function (){
-        $('.nav-collapse').css('height', '475px');
-       }); 
+       $(document).on('click', "#signup-link", function() {
+           $('.nav-collapse').css('height', '475px');
+       });
 
-     $(document).on('click', "#back-to-login-link", function (){
-        $('.nav-collapse').css('height', '405px');
-       }); 
-         
-         $(document).on('click', "#login-name-link", function (){
-        $('.nav-collapse').css('height', '395px');
-       }); 
+       $(document).on('click', "#back-to-login-link", function() {
+           $('.nav-collapse').css('height', '405px');
+       });
 
-    $(document).on('click', "#login-close-text", function (){
-        $('.nav-collapse').css('height', 'auto');
-       }); 
+       $(document).on('click', "#login-name-link", function() {
+           $('.nav-collapse').css('height', '395px');
+       });
 
-      $(document).on('click', "#login-buttons-open-change-password", function (){
-        $('.nav-collapse').css('height', '560');
-       }); 
+       $(document).on('click', "#login-close-text", function() {
+           $('.nav-collapse').css('height', 'auto');
+       });
 
-       $(document).on('click', "#login-buttons-do-change-password", function (){
-        $('.nav-collapse').css('height', 'auto');
-       }); 
+       $(document).on('click', "#login-buttons-open-change-password", function() {
+           $('.nav-collapse').css('height', '560');
+       });
 
-         $(document).on('click', "#login-buttons-logout", function (){
-        $('.nav-collapse').css('height', 'auto');
-       }); 
+       $(document).on('click', "#login-buttons-do-change-password", function() {
+           $('.nav-collapse').css('height', 'auto');
+       });
 
-        $(document).on('click', "#login-buttons-password", function (){
-        $('.nav-collapse').css('height', '305px');
-       }); 
+       $(document).on('click', "#login-buttons-logout", function() {
+           $('.nav-collapse').css('height', 'auto');
+       });
 
-        $(document).on('click', "#login-buttons .login-close-text", function (){
-        $('.nav-collapse').css('height', 'auto');
-       }); 
+       $(document).on('click', "#login-buttons-password", function() {
+           $('.nav-collapse').css('height', '305px');
+       });
 
+       $(document).on('click', "#login-buttons .login-close-text", function() {
+           $('.nav-collapse').css('height', 'auto');
+       });
 
     });
 
