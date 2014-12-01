db609ab7c88c6854d300dade6a0be69145cc8247
John Wallace <jwallace@appneta.com>
2014-11-30 22:47:00 -0500

    fix ui problem

--- client/helpers/nav-ui/nav.js
+++ client/helpers/nav-ui/nav.js
@@ -1,4 +1,4 @@
-  $(document).ready(function() {
+ $(document).ready(function() {
 
         // var screenwidth = $(window).width()
         // if (screenwidth <= 800) {
@@ -72,10 +72,45 @@
 
 
 
-     $(document).on("click", "login-buttons-logout", function() {
-        $('#loginModal').modal('hide');
-     });    
+       $(document).on('click', "#login-sign-in-link, .login-buttons", function() {
+           $('.nav-collapse').css('height', '405px');
+       });
 
+
+       $(document).on('click', "#signup-link", function() {
+           $('.nav-collapse').css('height', '475px');
+       });
+
+       $(document).on('click', "#back-to-login-link", function() {
+           $('.nav-collapse').css('height', '405px');
+       });
+
+       $(document).on('click', "#login-name-link", function() {
+           $('.nav-collapse').css('height', '395px');
+       });
+
+       $(document).on('click', "#login-close-text", function() {
+           $('.nav-collapse').css('height', 'auto');
+       });
+
+       $(document).on('click', "#login-buttons-open-change-password", function() {
+           $('.nav-collapse').css('height', '560');
+       });
+
+       $(document).on('click', "#login-buttons-do-change-password", function() {
+           $('.nav-collapse').css('height', 'auto');
+       });
+
+       $(document).on('click', "#login-buttons-logout", function() {
+           $('.nav-collapse').css('height', 'auto');
+       });
+
+       $(document).on('click', "#login-buttons-password", function() {
+           $('.nav-collapse').css('height', '305px');
+       });
+
+       $(document).on('click', "#login-buttons .login-close-text", function() {
+           $('.nav-collapse').css('height', 'auto');
+       });
 
     });
-
