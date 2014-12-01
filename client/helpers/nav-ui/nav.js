070425529ef7553c037488f76caa2deb3c34db46
John Wallace <jwallace@appneta.com>
2014-11-30 23:02:39 -0500

    merge in master

--- client/helpers/nav-ui/nav.js
+++ client/helpers/nav-ui/nav.js
@@ -100,62 +100,17 @@
         $('.nav-collapse').css('height', 'auto');
     });
 
-<<<<<<< HEAD
-    $(document).on('click', "#login-buttons-logout", function() {
-        $('.nav-collapse').css('height', 'auto');
-    });
-=======
-       $(document).on('click', "#login-sign-in-link, .login-buttons", function() {
-           $('.nav-collapse').css('height', '405px');
-       });
->>>>>>> fix-ui
 
     $(document).on('click', "#login-buttons-password", function() {
         $('.nav-collapse').css('height', '305px');
     });
 
-<<<<<<< HEAD
-    $(document).on('click', "#login-buttons .login-close-text", function() {
-        $('.nav-collapse').css('height', 'auto');
-    });
+   $(document).on('click', "#login-buttons-logout", function() {
+       $('.nav-collapse').css('height', 'auto');
+   });
 
-});
-=======
-       $(document).on('click', "#signup-link", function() {
-           $('.nav-collapse').css('height', '475px');
-       });
+   $(document).on('click', "#login-buttons .login-close-text", function() {
+       $('.nav-collapse').css('height', 'auto');
+   });
+ });
 
-       $(document).on('click', "#back-to-login-link", function() {
-           $('.nav-collapse').css('height', '405px');
-       });
-
-       $(document).on('click', "#login-name-link", function() {
-           $('.nav-collapse').css('height', '395px');
-       });
-
-       $(document).on('click', "#login-close-text", function() {
-           $('.nav-collapse').css('height', 'auto');
-       });
-
-       $(document).on('click', "#login-buttons-open-change-password", function() {
-           $('.nav-collapse').css('height', '560');
-       });
-
-       $(document).on('click', "#login-buttons-do-change-password", function() {
-           $('.nav-collapse').css('height', 'auto');
-       });
-
-       $(document).on('click', "#login-buttons-logout", function() {
-           $('.nav-collapse').css('height', 'auto');
-       });
-
-       $(document).on('click', "#login-buttons-password", function() {
-           $('.nav-collapse').css('height', '305px');
-       });
-
-       $(document).on('click', "#login-buttons .login-close-text", function() {
-           $('.nav-collapse').css('height', 'auto');
-       });
-
-    });
->>>>>>> fix-ui
